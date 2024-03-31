339d298f81ed5f0fe6761b2295ea4f4ccf7b57f8
CALEB KOMEN <komencaleb502@gmail.com>
2024-04-01 00:08:49 +0300

    dom

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("This is really cool!");
+  
+    // Get the <p> element
+    const paragraphElement = document.querySelector('p');
+    
+    // Change the text content
+    paragraphElement.textContent = "This is really cool!";
+  });
